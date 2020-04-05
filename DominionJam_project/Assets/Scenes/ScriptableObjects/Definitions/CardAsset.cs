a5cf3e94df974b0f13c39d23726711e103ddcbc0
Jacob <its_thrasher@live.ca>
2020-04-05 13:27:15 -0400

    Add basic scriptable objects to represent cards

---
+++ DominionJam_project/Assets/Scenes/ScriptableObjects/Definitions/CardAsset.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+public class CardAsset : ScriptableObject
+{
+	[SerializeField]
+	private string name;
+
+	[SerializeField]
+	private int actions;
+
+	[SerializeField]
+	private int cards;
+
+	[SerializeField]
+	private CardBehaviourAsset behaviourAsset;
+
+	public string Name => name;
+	public int Actions => actions;
+	public int Cards => cards;
+	public System.Action Behaviour => behaviourAsset.GetExecutableBehaviour();
+}
