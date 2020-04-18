22ad15e727c2d7e59a366a21a7b143f7c6d27cea
Jacob <its_thrasher@live.ca>
2020-04-18 19:58:25 -0400

    Empty player class

---
+++ DominionJam_project/Assets/_Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Domin
+{
+	public class Player : MonoBehaviour
+	{
+		public enum PlayStates
+		{
+
+		}
+	}
+}
