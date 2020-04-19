fd950bbd9ef90fbc9dff0a2bfeb8836d336d4dd7
Jacob <its_thrasher@live.ca>
2020-04-19 13:38:43 -0400

    Start card importer

---
+++ DominionJam_project/Assets/_Scripts/Editor/CardImportWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CardImportWindow : EditorWindow
+{
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Window/Card Importer")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        CardImportWindow window = (CardImportWindow)GetWindow(typeof(CardImportWindow));
+        window.Show();
+    }
+
+    //private void OnGUI()
+    //{
+    //    GUILayout.
+    //}
+}
